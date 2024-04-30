26d45b36c772d1d3686269a008b4fbe2d41454de
Ángel Becerra <angelmirocu@gmail.com>
2024-04-29 20:34:27 -0600

    Primeras pruebas

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace AjedrezVisual
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Location" + button1.Location.X.ToString() +"\n"+button1.Location.Y.ToString());
+            //button1.Image = Image.FromFile("C:\\Users\\Usuario\\source\\repos\\AjedrezVisual\\AjedrezVisual\\Resources\\alfil.png");
+            
+            Application.Exit();
+
+
+        }
     }
 }
