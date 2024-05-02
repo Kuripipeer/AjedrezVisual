dc14d1a4d363d651d8289623d439a9f59785ccba
Ángel Becerra <angelmirocu@gmail.com>
2024-05-01 19:15:25 -0600

    Update

--- Form1.cs
+++ Form1.cs
@@ -15,63 +15,6 @@ namespace AjedrezVisual
         private void button1_Click(object sender, EventArgs e)
         {
             Application.Restart();
-            foreach (PictureBox picture in panel1.Controls.OfType<PictureBox>().Reverse<PictureBox>())
-            {
-                switch (picture.Name)
-                {
-                    case "pictureBox1":
-                        picture.Image = Resource1.White_Rook;
-                        break;
-                    case "pictureBox2":
-                        picture.Image = Resource1.White_Knight;
-                        break;    
-                    case "pictureBox3":
-                            picture.Image = Resource1.White_Bishop;
-                        break;
-                    case "pictureBox4":
-                            picture.Image = Resource1.White_Queen;
-                        break;
-                    case "pictureBox5":
-                            picture.Image = Resource1.White_King;
-                        break;
-                    case "pictureBox6":
-                            picture.Image = Resource1.White_Bishop;
-                        break;
-                    case "pictureBox7":
-                            picture.Image = Resource1.White_Knight;
-                        break;
-                        case "pictureBox8":
-                            picture.Image = Resource1.White_Rook;
-                        break;
-                        case "pictureBox9":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox10":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox11":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox12":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox13":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox14":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox15":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                        case "pictureBox16":
-                            picture.Image = Resource1.White_Pawn;
-                        break;
-                    default:
-                        picture.Image = null;
-                        break;
-                }
-            }
         }
 
         public void CrearTablero()
