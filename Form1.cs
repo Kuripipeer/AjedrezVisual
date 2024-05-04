c33d00e5db2ba9699306926f2689c166d038ffd2
Ángel Becerra <angelmirocu@gmail.com>
2024-05-04 10:11:18 -0600

    update

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,13 @@
 using AjedrezVisual.Properties;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace AjedrezVisual
 {
     public partial class Form1 : Form
     {
+        int seleccion = 0;
         string[,] tablero = new string[8, 8];
 
         public Form1()
@@ -48,13 +51,34 @@ namespace AjedrezVisual
             //    }
             //}
         }
-        
+
         private void SeleccionaPieza(object sender, EventArgs e)
         {
             // Obtiene la posición de un control PictureBox
             Point position = GetPictureBoxPosition(sender);
-            MessageBox.Show(position.ToString());
-            
+            if (sender is PictureBox pictureBox)
+            {
+                //Stream imageStream = Properties.Resources.ResourceManager.GetObject("Black_Rook").GetType().Assembly.GetManifestResourceStream("myImage.png");
+                if (pictureBox.Image != null)
+                {
+                    string fileName = pictureBox.Tag.ToString();
+                    MessageBox.Show("Filename: " + fileName);
+                    if (fileName.Contains("White"))
+                    {
+                        MessageBox.Show("Pieza blanca");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Racista");
+                    }
+
+                    seleccion = 1;
+                }
+                else if (seleccion == 0)
+                {
+                    MessageBox.Show("Selecciona una pieza a mover.");
+                }
+            }
         }
 
 
@@ -65,6 +89,11 @@ namespace AjedrezVisual
             return point;
         }
 
-
+        private void CasillaDestino(object sender, EventArgs e)
+        {
+            // Obtiene la posición de un control PictureBox
+            Point position = GetPictureBoxPosition(sender);
+            MessageBox.Show("Posición final "+position.ToString());
+        }
     }
 }
